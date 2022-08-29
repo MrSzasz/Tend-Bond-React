3428d7536156c2c10fd950a09340e551308a80db
MrSzasz <lugoleandro@hotmail.com>
2022-08-29 19:27:41 -0300

    tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      main: '#F48F4A'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
