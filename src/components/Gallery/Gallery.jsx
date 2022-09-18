778c62b541f524f8308dd56033bdbd2846e4f7bd
MrSzasz <lugoleandro@hotmail.com>
2022-09-18 17:14:07 -0300

    gallery added and merged

---
+++ src/components/Gallery/Gallery.jsx
@@ -0,0 +1,38 @@
+import ReactImageGallery from "react-image-gallery";
+import "./Gallery.scss";
+
+const Gallery = () => {
+  const images = [
+    {
+      original: "https://picsum.photos/id/1018/1000/600/",
+      thumbnail: "https://picsum.photos/id/1018/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1015/1000/600/",
+      thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1019/1000/600/",
+      thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1019/1000/600/",
+      thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1019/1000/600/",
+      thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1019/1000/600/",
+      thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1019/1000/600/",
+      thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+  ];
+  return <ReactImageGallery items={images} thumbnailPosition={"left"} infinite={true} showNav={false} useBrowserFullscreen={false} showPlayButton={false} />;
+};
+
+export default Gallery;
