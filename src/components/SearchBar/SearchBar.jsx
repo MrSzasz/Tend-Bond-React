9ce34556de1fd36e627fcad8e345fcac6d1fbbfa
MrSzasz <lugoleandro@hotmail.com>
2022-10-12 12:06:32 -0300

    back to window location

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,17 +1,15 @@
 import "./SearchBar.scss";
 import { FiSearch } from "react-icons/fi";
 import ReactSearchBox from "react-search-box";
-import { useNavigate } from "react-router-dom";
 
 const SearchBar = ({ array }) => {
-  const navigate = useNavigate()
   return (
     <div className="flex gap-2 items-center h-full w-48">
       <ReactSearchBox
         placeholder="¡Hola! ¿Qué buscas?"
         iconBoxSize="30px"
         onSelect={(record) =>
-          navigate(`https://trend-bond-react.netlify.app/products/detalle/${record.item.key}`)
+          (window.location.href = `https://trend-bond-react.netlify.app/products/detalle/${record.item.key}`)
         }
         data={array}
         leftIcon={<FiSearch size={20} />}
